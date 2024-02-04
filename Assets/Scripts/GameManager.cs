a14d4e1d34ed9cd0b0574f83fd8e5cf7a8ca0b7d
Siddube <prajwal.be.21@gmail.com>
2024-02-04 19:13:07 +0530

    Init Commit, Init Project Setup, Init Start Menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{ 
+
+}
+
