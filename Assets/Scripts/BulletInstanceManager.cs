87301aaf3fe33d9e00db6497930391406d52b186
Siddube <prajwal.be.21@gmail.com>
2024-02-09 17:16:34 +0530

    Clean up code, Comment - Bullet Classes

--- Assets/Scripts/BulletInstanceManager.cs
+++ Assets/Scripts/BulletInstanceManager.cs
@@ -1,43 +1,46 @@
 /* ------------------------------------------------------------------------------
-<BulletInstanceManager Class>
-  This script handles
+BulletInstanceManager Class
+  * This script handles
   1> Playing bullet sound fx
   2> Moving bullets
   3> Destory bullets if out of screen bounds
-<BulletInstanceManager Class>
+  4> Play sound fx of firing bullets
 --------------------------------------------------------------------------------*/
-using System;
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
-using UnityEngine.Events;
 
 public class BulletInstanceManager : MonoBehaviour
 {
+    // Private property to reference the player space ship game object
     private GameObject m_player;
+    // Private property to reference the player combat script on the player game object
     private PlayerCombatManager m_playerCombatManager;
-    //Event emitted when bullet destroys an asteroid
-    //public UnityEvent ShotAsteroid;
-    // Property to set speed of bullets
+    // private property to set speed of bullets
     private float m_speed = 10f;
     // Reference to audio source with bullet shot sound fx
-    private AudioSource audioSource;
+    private AudioSource m_audioSource;
 
-
+    // Awake method
     private void Awake()
     {
+        // Set property to player spaceship instance with tag
         m_player = GameObject.FindGameObjectWithTag("Player");
+        // Set reference to player combat script 
         m_playerCombatManager = m_player.GetComponent<PlayerCombatManager>();
-        // Reference the audio source component
-        audioSource = this.gameObject.GetComponent<AudioSource>();
+        // Set reference the audio source component on the bullet instance
+        m_audioSource = this.gameObject.GetComponent<AudioSource>();
     }
 
+    // Start method
     private void Start()
     {
-        if (!audioSource) { Debug.Log("ERR: BulletInstanceManager ====== Start() ====== Audio Source Not Found"); return; }
-        // Play audio bullet fired clip
-        audioSource.Play();
+        // Check if audio source property is null and log warning
+        if (!m_audioSource) { Debug.Log("ERR: BulletInstanceManager ====== Start() ====== Audio Source Not Found"); return; }
+        // Play audio bullet fired audio fx
+        m_audioSource.Play();
     }
+
+    // Update method
     private void Update()
     {
         // Call move bullets method
@@ -46,33 +49,33 @@ public class BulletInstanceManager : MonoBehaviour
         DestroyOnOutOfScreenBounds();
     }
 
+    // Move bullets method
     private void MoveBullets()
     {
         // Move bullet in the forward direction with value of the speed property
         this.gameObject.transform.Translate(Vector3.up * m_speed * Time.deltaTime);
     }
 
+    // OnTriggerEnter method
     private void OnTriggerEnter(Collider other)
     {
         // Trigger enter method
         if (other.gameObject.tag == "Asteroids")
         {
-            m_playerCombatManager.UpdateScore(m_playerCombatManager.Score, 100);
             // Asteroid shot
-            // Invoke asteroid shot event 
-            //ShotAsteroid.Invoke();
-            // Set the asteroid instance game object to false
-            //other.gameObject.SetActive(false);
-            // Destroy the game object
-            //Destroy(other.gameObject);
+            if (!m_playerCombatManager) { Debug.Log("ERR: BulletInstanceManager ====== OnTriggerEnter() ====== Player Combat Script Not Found"); return; }
+            m_playerCombatManager.UpdateScore(m_playerCombatManager.Score, 100);
+
+            // Call destroy asteroid fx function to play asteroid destroyed sound and particle fx
             other.GetComponent<AsteroidInstaceManager>().DestroyAsteroidFx();
+            // Call destroy asteroid method to destroy asteroid game object
             other.GetComponent<AsteroidInstaceManager>().DestroyAsteroid();
             // Call destroy bullet instance method
             DestroyBulletInstance();
-            return;
         }
     }
 
+    // DestroyOnOutOfScreenBounds method
     private void DestroyOnOutOfScreenBounds()
     {
         // Calculate viewport position of bullet
@@ -87,7 +90,7 @@ public class BulletInstanceManager : MonoBehaviour
             DestroyBulletInstance();
         }
     }
-
+    // DestroyBulletInstance method
     private void DestroyBulletInstance()
     {
         // Method to destroy bullet instance
--- Assets/Scripts/BulletSpawnManager.cs
+++ Assets/Scripts/BulletSpawnManager.cs
@@ -1,39 +1,35 @@
 /* ------------------------------------------------------------------------------
-<BulletSpawnManager Class>
-  This script handles
-  1> Spawning of new bullets on pressing spacbar
-<BulletSpawnManager Class>
+BulletSpawnManager Class
+  *This script handles
+  1> Spawning of new bullets
 --------------------------------------------------------------------------------*/
-using System;
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
-using UnityEngine.Events;
 
 public class BulletSpawnManager : MonoBehaviour
 {
 
-    // Private property accessible from editor, to reference bullet prefab
-    [field: SerializeField] private GameObject BulletPrefab;
-    // Private property accessible from editor, to reference bullet prefab's parent object
-    [field: SerializeField] private GameObject BulletParentObject;
-    // Private property accessible from editor, to reference the transform where new bullets are instantiated
-    [field: SerializeField] private Transform BulletSpawnTransform;
-    // Private property that references the new bullet instantiated
-    private GameObject m_newBullet;
+  // Private property accessible from editor, to reference bullet prefab
+  [SerializeField] private GameObject BulletPrefab;
+  // Private property accessible from editor, to reference bullet prefab's parent object
+  [SerializeField] private GameObject BulletParentObject;
+  // Private property accessible from editor, to reference the transform where new bullets are instantiated
+  [SerializeField] private Transform BulletSpawnTransform;
+  // Private property that references the new bullet instantiated
+  private GameObject m_newBullet;
 
-    public void SpawnBullet()
-    {
-        // Spawn new bullets when the shoot event is invoked from player combat manager class
-        // If bullet prefab is not referenced, then throw a warning to the console log
-        if (!BulletPrefab) { Debug.Log("ERR: SpaceshipCombat ====== ShootRegular() ====== Bullet Prefab Not Found"); return; }
-        // If bullet prefab parent object is not referenced, then throw a warning to the console log
-        if (!BulletParentObject) { Debug.Log("ERR: SpaceshipCombat ====== ShootRegular() ====== Bullet Parent Object Not Found"); return; }
+  public void SpawnBullet()
+  {
+    // Spawn new bullets when the shoot event is invoked from player combat manager class
+    // If bullet prefab is not referenced, then throw a warning to the console log
+    if (!BulletPrefab) { Debug.Log("ERR: SpaceshipCombat ====== ShootRegular() ====== Bullet Prefab Not Found"); return; }
+    // If bullet prefab parent object is not referenced, then throw a warning to the console log
+    if (!BulletParentObject) { Debug.Log("ERR: SpaceshipCombat ====== ShootRegular() ====== Bullet Parent Object Not Found"); return; }
 
-        // Else, instantiate a new bullet from bullet prefab
-        // Using bullet spawn transform
-        m_newBullet = Instantiate(BulletPrefab, BulletSpawnTransform.position, BulletSpawnTransform.rotation);
-        // Set the bullet parent object to the transform of the spawn position
-        m_newBullet.transform.parent = BulletParentObject.transform;
-    }
+    // Else, instantiate a new bullet from bullet prefab
+    // Using bullet spawn transform
+    m_newBullet = Instantiate(BulletPrefab, BulletSpawnTransform.position, BulletSpawnTransform.rotation);
+    // Set the bullet parent object to the transform of the spawn position
+    m_newBullet.transform.parent = BulletParentObject.transform;
+  }
 }
