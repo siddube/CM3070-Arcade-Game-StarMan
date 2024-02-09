11fb1a2d7b948ce58704dd4fe664873a64b2c2c3
Siddube <prajwal.be.21@gmail.com>
2024-02-09 18:25:56 +0530

    Clean up code, Comment - Health Bar

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -1,26 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    [field: SerializeField] private Slider m_slider;
-    [field: SerializeField] private Gradient m_gradient;
-    [field: SerializeField] private Image m_fillImage;
+    // Property set from editor, to reference slider used to set health
+    [SerializeField] private Slider m_slider;
+    // Property set from editor, to reference health gradient
+    [SerializeField] private Gradient m_gradient;
+    // Property set from editor, to reference fill image
+    [SerializeField] private Image m_fillImage;
+    // Property set from editor, to reference player combat manager
     [SerializeField] public PlayerCombatManager playerCombatManager;
 
+    // Start method
+    private void Start()
+    {
+        if (m_slider == null) { Debug.Log("ERR: HealthBar ====== Start() ====== Slider Component Not Found"); return; }
+        if (m_gradient == null) { Debug.Log("ERR: HealthBar ====== Start() ====== Gradient Component Not Found"); return; }
+        if (m_fillImage == null) { Debug.Log("ERR: HealthBar ====== Start() ====== Fill Image Component Not Found"); return; }
+        if (playerCombatManager == null) { Debug.Log("ERR: HealthBar ====== Start() ====== Player Combat Manager Not Found"); return; }
+    }
+
+    // SetMaxHealth method
     public void SetMaxHealth(int health)
     {
-        if (m_slider == null) { Debug.Log("ERR: HealthBar ====== SetMaxHealth() ====== Slider Component Not Found"); return; }
+        // Setup intial value on the health slider
+        // Set max health value
         m_slider.maxValue = health;
+        // Set current health value
         m_slider.value = health;
+        // Set fill colour
         m_fillImage.color = m_gradient.Evaluate(1f);
     }
 
+    // SetHealth method
     public void SetHealth(int health)
     {
+        // Set current health value
         m_slider.value = health;
+        // Set fill colour
         m_fillImage.color = m_gradient.Evaluate(m_slider.normalizedValue);
     }
 }
