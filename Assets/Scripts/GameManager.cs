1b6c56f4d20de994ef0bce30ef462140bc3ec8c8
Siddube <prajwal.be.21@gmail.com>
2024-02-09 15:41:38 +0530

    Clean up code, Comment - Game Manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,21 +1,16 @@
 /* ------------------------------------------------------------------------------
-<GameManager Class>
+GameManager Class
   This script is the game manager that handles
   1> Game states reference
   2> Changes game states
   3> The flow of game states
-<GameManager Class>
+GameManager Class
 --------------------------------------------------------------------------------*/
-using System;
+
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
-
 
 public class GameManager : MonoBehaviour
 {
@@ -24,33 +19,35 @@ public class GameManager : MonoBehaviour
   private bool m_setupGameScene = false;
   // Public property other classes can get and set
   public bool SetupGameScene { get => m_setupGameScene; set => m_setupGameScene = value; }
-
   // Private bool property to to know if the level has started
   private bool m_hasGameStarted = false;
   // Public property other classes can get and set
   public bool HasGameStarted { get => m_hasGameStarted; set => m_hasGameStarted = value; }
-
   // Private bool property to know if the game is in progress
   private bool m_isGamePlaying = false;
   // Public property other classes can get and set
   public bool IsGamePlaying { get => m_isGamePlaying; set => m_isGamePlaying = value; }
-
   // Private bool property to know if the game is over
   private bool m_isGameOver = false;
+  // Public property other classes can get and set
   public bool IsGameOver { get => m_isGameOver; set => m_isGameOver = value; }
+  // Private bool property to know if the game time is over
   private bool m_isTimeOver = false;
+  // Public property other classes can get and set
   public bool IsTimeOver { get => m_isTimeOver; set => m_isTimeOver = value; }
 
-
   // References to other game objects used in the script
+  // Reference to the player space ship game object
   private GameObject m_player;
 
-
   // Properties to save coroutine names
+  // Run main game loop
   private string RunGameLoopString = "RunGameLoopRoutine";
+  // Run setup game scene co-routine
   private string SetupGameSceneString = "SetupGameSceneRoutine";
-
+  // Run play game co-routine
   private string PlayGameString = "PlayGameRoutine";
+  // Run end game co-routine
   private string EndGameString = "EndGameRoutine";
 
   // Events emitted by game manager
@@ -60,12 +57,14 @@ public class GameManager : MonoBehaviour
 
   private void Awake()
   {
-    // Player Reference
+    //Awake method
+    // Get player reference
     m_player = GameObject.FindGameObjectWithTag("Player");
   }
 
   private void Start()
   {
+    // Start method
     // Check if a player reference has been setup
     if (m_player != null)
     {
@@ -83,13 +82,15 @@ public class GameManager : MonoBehaviour
     // Coroutines that control flow of game states
     // Coroutine to setup game scene
     yield return StartCoroutine(SetupGameSceneString);
+    // Coroutine to play game
     yield return StartCoroutine(PlayGameString);
+    // Coroutine to end game
     yield return StartCoroutine(EndGameString);
-
   }
 
   IEnumerator SetupGameSceneRoutine()
   {
+    // Setup game scene co-routine
     // Setup game scene method is used to set the game scene and display start menu
     Debug.Log("GameManager info: Setup Game Scene");
     // If the setup game scene event property is not null
@@ -104,17 +105,18 @@ public class GameManager : MonoBehaviour
         yield return null;
       }
     }
-    // Once the while loop exits on starting the game
+    // Once the while loop exits on pressing play button
     // Check if start game event property is not null
     if (StartGameEvent != null)
     {
-      //Then invoke start game event for other game objects to follow 
+      // Then invoke start game event for other game objects to follow 
       StartGameEvent.Invoke();
     }
   }
 
   IEnumerator PlayGameRoutine()
   {
+    // Play game co-routine
     Debug.Log("GameManager info: Playing Game");
     // Set the game has started bool to true
     m_hasGameStarted = true;
@@ -124,15 +126,19 @@ public class GameManager : MonoBehaviour
     {
       yield return null;
     }
+    // Check if end game event property is not null
     if (EndGameEvent != null)
     {
+      // Then invoke end game event for other game objects to follow 
       EndGameEvent.Invoke();
     }
   }
 
   IEnumerator EndGameRoutine()
   {
+    // End game co-routine
     Debug.Log("GameManager info: Ending Game");
+    // Yiield null when the game is over
     while (m_isGameOver || m_isTimeOver)
     {
       yield return null;
@@ -141,7 +147,10 @@ public class GameManager : MonoBehaviour
 
   public void RestartLevel()
   {
+    // Restart game level method
+    // Get the current active scene
     Scene scene = SceneManager.GetActiveScene();
+    // Load scene again to reboot game
     SceneManager.LoadScene(scene.name);
   }
 }
