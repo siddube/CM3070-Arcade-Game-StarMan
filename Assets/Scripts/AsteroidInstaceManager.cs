0229392b58d629c6ee93b4ffd4d9cf25bdb3651c
Siddube <prajwal.be.21@gmail.com>
2024-02-19 13:12:42 +0530

    Clean up Code, Comment - Asteroid Classes

--- Assets/Scripts/AsteroidInstaceManager.cs
+++ Assets/Scripts/AsteroidInstaceManager.cs
@@ -14,12 +14,12 @@ public class AsteroidInstaceManager : MonoBehaviour
 
 	// Private property that references player game object
 	private GameObject m_player;
-	// Private property that references player combat manager
+	// Private property that references player combat script
 	private PlayerCombatManager m_playerCombatManager;
 	// Private property that references player spaceship body collider
-	//
-	private EnemySpaceshipInstanceCombatManager m_enemySpaceshipInstanceCombatManager;
 	private Collider m_playerShipCollider;
+	// Private property that references enemy spacehip instance combat script
+	private EnemySpaceshipInstanceCombatManager m_enemySpaceshipInstanceCombatManager;
 	// Private property that references asteroid particle system fx
 	private ParticleSystem m_particleSystemInstance;
 	// Private property that references asteroid audio fx
@@ -28,10 +28,8 @@ public class AsteroidInstaceManager : MonoBehaviour
 	private Collider m_collider;
 	// Private property that references mesh renderer on the asteroid instance
 	private MeshRenderer m_meshRenderer;
-	// Private property to reference the delay destroy co-routine
-	private string DelayDestroyAsteroidString = "DelayDestroyAsteroidRoutine";
 	// Private property to reference the delay in destroying asteroid game object 
-	private float m_destroyDelay = 0.7f;
+	private float m_destroyDelay = 3f;
 
 	// Awake method
 	private void Awake()
@@ -40,8 +38,9 @@ public class AsteroidInstaceManager : MonoBehaviour
 		m_player = GameObject.FindGameObjectWithTag("Player");
 		// Set reference to player combat script on the player game object 
 		m_playerCombatManager = m_player.GetComponent<PlayerCombatManager>();
+		// Set reference to collider om the player game object 
 		m_playerShipCollider = m_player.GetComponent<CapsuleCollider>();
-		//
+		// Set reference to enemy ship combat script
 		m_enemySpaceshipInstanceCombatManager = GameObject.FindAnyObjectByType<EnemySpaceshipInstanceCombatManager>();
 		// Set reference to audio source on the asteroid instace 
 		m_audioSource = this.gameObject.GetComponent<AudioSource>();
@@ -60,7 +59,7 @@ public class AsteroidInstaceManager : MonoBehaviour
 	{
 		if (m_playerCombatManager == null) { Debug.Log("ERR: AsteroidInstaceManager ====== OnTriggerEnter() ====== Player Combat Script Not Found"); return; }
 		// Collider Trigger Method
-		// Check if the object collided with is player
+		// Check if the asteroid collided with is player
 		if (other == m_playerShipCollider)
 		{
 			// Yes, then set damage to player
@@ -70,10 +69,16 @@ public class AsteroidInstaceManager : MonoBehaviour
 			// Call Destroy asteroid method 
 			DestroyAsteroid();
 		}
+		// Check if the asteroid collided with enemy
 		if (other.gameObject.tag == "Enemy")
 		{
+			// Check if enemy still has combat script attached and not destoryed
+			if (m_enemySpaceshipInstanceCombatManager == null) { Debug.Log("ERR: AsteroidInstaceManager ====== OnTriggerEnter() ====== Enemy Combat Script Not Found"); return; }
+			// Yes, then set damage to player
 			m_enemySpaceshipInstanceCombatManager.TakeDamage(m_enemySpaceshipInstanceCombatManager.CurrentHealth, 10);
+			// Call destory to play asteroid collision particle fx and sound fx
 			DestroyAsteroidFx();
+			// Call Destroy asteroid method 
 			DestroyAsteroid();
 		}
 	}
@@ -83,7 +88,7 @@ public class AsteroidInstaceManager : MonoBehaviour
 	{
 		// Method to destroy asteroid instance
 		// If called destroy the asteroid instance
-		StartCoroutine(DelayDestroyAsteroidString);
+		StartCoroutine("DelayDestroyAsteroidRoutine");
 	}
 
 	// DelayDestroyAsteroidRoutine method
